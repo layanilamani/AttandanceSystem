eff625f75c11f9505b9e47fc4fcf539deb7f3fba
layanilamani <layani.lamani@gmail.com>
2017-12-06 10:46:24 +0500

    adding new project for mini attandance system based upon ajax, mvc C#

---
+++ WebApplication9/Controllers/BatchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MiniAttandanceSystem.Controllers
+{
+    public class BatchController : ApiController
+    {
+        MiniAttandanceSystemEntities db = new MiniAttandanceSystemEntities();
+
+        [Route("api/GetAllBatches")]
+        public List<Batch> GetAllBatches()
+        {
+            return db.Batches.ToList();
+        }
+
+        //public List<Student> GetStudntsByBatch(string batch)
+        //{
+           
+        //}
+    }
+}
